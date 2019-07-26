a4d7218541fd52b15d0cae7e1ef513f2bd512287
EdwinHoenselaar <EdwinHoenselaar@gmail.com>
2019-07-26 15:55:38 +0200

    push for code review

--- word/routes.js
+++ word/routes.js
@@ -25,65 +25,20 @@ router.get('/words', (req, res, next) => {
     .catch(error => next(error))
 })
 
-router.get('/words/:date', (req, res, next) => {
+router.post('/word', (req, res) => {
   Word
-  findAll({
-    where: {
-        date: req.params.date
-    },
-    attributes: ['id', 'word', 'date'], //object
-})    .then(word => {
-      if (!word) {
-        return res.status(404).send({
-          message: `Word does not exist`
-        })
-      }
-      return res.send(word)
+    .create({
+      word: req.body.word,
+      date: new Date().toJSON().slice(0,10),
+      description: req.body.description
     })
-    .catch(error => next(error))
-})
-
-router.post('/words', (req, res) => {
-  Word
-    .create(req.body)
     .then(word => {
       if (!word) {
         return res.status(404).send({
-          message: 'Word does not exist'
+          message: 'Word not created'
         })
       }
     })
 })
-
-// router.post('/advertisements', (req, res, next) => {
-//   Advertisement
-//     .create(req.body)
-//     .then(advertisement => {
-//       if (!advertisement) {
-//         return res.status(404).send({
-//           message: `Advertisement does not exist`
-//         })
-//       }
-//       return res.status(201).send(advertisement)
-//     })
-//     .catch(error => next(error))
-// })
-
-// router.delete('/advertisements/:id', (req, res, next) => {
-//   Advertisement
-//     .findByPk(req.params.id)
-//     .then(advertisement => {
-//       if (!advertisement) {
-//         return res.status(404).send({
-//           message: `Advertisement does not exist`
-//         })
-//       }
-//       return advertisement.destroy()
-//         .then(() => res.send({
-//           message: `Advertisement was deleted`
-//         }))
-//     })
-//     .catch(error => next(error))
-// })
 
 module.exports = router
